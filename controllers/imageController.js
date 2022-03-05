19ee036aa0e014e878641d15dc9b1bd6fd7b1df3
kiendao515 <daotrungkien515@gmail.com>
2022-03-05 21:51:13 +0700

    deploy lai

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,70 +1,87 @@
-const {Image} = require('../model/image');
+const { Image } = require('../model/image');
 const jwt = require("jsonwebtoken");
 const { User } = require('../model/user');
 const { createJwtToken } = require("../util/auth")
 const mongoose = require("mongoose");
-const addImage = async(req,res,next)=>{
-    const {imageUrl,description,tag}= req.body;
+const addImage = async (req, res, next) => {
+    const { imageUrl, description, tag } = req.body;
     console.log(tag);
     const token = req.headers.authorization.split(' ')[1];
-        let id ;
-        if (token) {
-            jwt.verify(token, "kiendao2001", function (err, decodedToken) {
-                if (err) {
-                    return res.json({ status: 'fail', msg: "Invalid token" })
-                }
-                id= decodedToken.userID;
-            });
-            if(id){
-                let user= await User.findOne({_id:id});
-                if(user){
-                    let image = new Image({userId:user._id,imageUrl:imageUrl,description:description,tag:tag})
-                    image.save().then(doc=>{
-                        return res.json({status:'success',data:doc})
-                    })
-                }
+    let id;
+    if (token) {
+        jwt.verify(token, "kiendao2001", function (err, decodedToken) {
+            if (err) {
+                return res.json({ status: 'fail', msg: "Invalid token" })
+            }
+            id = decodedToken.userID;
+        });
+        if (id) {
+            let user = await User.findOne({ _id: id });
+            if (user) {
+                let image = new Image({ userId: user._id, imageUrl: imageUrl, description: description, tag: tag })
+                image.save().then(doc => {
+                    return res.json({ status: 'success', data: doc })
+                })
             }
         }
+    }
 }
 
-const getListImage = async(req,res,next)=>{
-    if(req.headers.authorization){
+const getListImage = async (req, res, next) => {
+    if (req.headers.authorization) {
         const token = req.headers.authorization.split(' ')[1];
-        let id ;
+        let id;
         if (token) {
             jwt.verify(token, "kiendao2001", function (err, decodedToken) {
                 if (err) {
                     return res.json({ status: 'fail', msg: "Invalid token" })
                 }
-                id= decodedToken.userID;
+                id = decodedToken.userID;
             });
-            if(id){
-                let user= await User.findOne({_id:id});
-                if(user){
-                    Image.find({userId:user._id}).populate('tag').then(doc=>{
-                        return res.json({status:'success',data:doc})
+            if (id) {
+                let user = await User.findOne({ _id: id });
+                if (user) {
+                    Image.find({ userId: user._id }).populate('tag').then(doc => {
+                        return res.json({ status: 'success', data: doc })
                     })
                 }
             }
         }
-    }else{
-        return res.json({status:'fail',msg:'token required!'})
+    } else {
+        return res.json({ status: 'fail', msg: 'token required!' })
     }
 }
 
-const listDataFromFilter = async(req,res,next)=>{
-    let {tag}= req.body;
-    console.log(tag);
-    let arr=[];
-    for(var i=0;i< tag.length;i++){
-        arr.push(mongoose.Types.ObjectId(tag[i]));
-        if(arr.length===tag.length){
-            Image.find({'tag':{$in:tag}}).then(doc=>{
-                return res.json({status:'success',data:doc})
-            })
+const listDataFromFilter = async (req, res, next) => {
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(' ')[1];
+        let id;
+        if (token) {
+            jwt.verify(token, "kiendao2001", function (err, decodedToken) {
+                if (err) {
+                    return res.json({ status: 'fail', msg: "Invalid token" })
+                }
+                id = decodedToken.userID;
+            });
+            if (id) {
+                let user = await User.findOne({ _id: id });
+                if (user) {
+                    let { tag } = req.body;
+                    console.log(tag);
+                    let arr = [];
+                    for (var i = 0; i < tag.length; i++) {
+                        arr.push(mongoose.Types.ObjectId(tag[i]));
+                        if (arr.length === tag.length) {
+                            Image.find({ userId:user._id,'tag': { $in: tag } }).then(doc => {
+                                return res.json({ status: 'success', data: doc })
+                            })
+                        }
+                    }
+                }
+            }
         }
     }
 }
-exports.addImage=addImage;
-exports.getListImage=getListImage;
-exports.listDataFromFilter=listDataFromFilter;
+exports.addImage = addImage;
+exports.getListImage = getListImage;
+exports.listDataFromFilter = listDataFromFilter;
